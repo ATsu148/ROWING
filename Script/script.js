bfe120ca42f2f9ca6268e8d7a8e8296380bccbf3
koshiatuyaki <80621343+koshiatuyaki@users.noreply.github.com>
2021-04-03 20:25:38 +0900

    new Files

---
+++ Script/script.js
@@ -0,0 +1,15 @@
+$(window).on("resize", function() {
+    var w = $(window).width();
+    if(w < 1153) {
+      window.location = window.location;
+    }
+    if(w < 800) {
+      window.location = window.location;
+    }
+    if(w < 600) {
+      window.location = window.location;
+    }
+    if(w < 480) {
+      window.location = window.location;
+    }
+  });
